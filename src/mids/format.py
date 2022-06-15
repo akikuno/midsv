18c7eed1a1bef1612c8db3924c9f38dd08e39be8
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-06-16 06:52:49 +0900

    add docstrings

--- src/mids/format.py
+++ src/mids/format.py
@@ -2,7 +2,16 @@ from __future__ import annotations
 
 import re
 
-def append_reference_sequence_length(headers: list[dict], alignments: list[dict]):
+def append_reference_sequence_length(headers: list[dict], alignments: list[dict]) -> list[dict]:
+    """Append reference sequence length
+
+    Args:
+        headers (list[dict]): headers including SQ (reference name) and LN (reference sequence length)from SAM
+        alignments (list[dict]): alignments from SAM
+
+    Returns:
+        list[dict]: alignments appended LN named as "RLEN"
+    """
     alingment_with_reference_sequence_length = []
     for alignment in alignments:
         alignment["RLEN"] = headers[alignment["RNAME"]]
