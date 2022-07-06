1ee65d5f30e9bd6cf728d785882ad32b6e3bf988
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-07-06 11:38:51 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="mids",
+    version="0.1.0",
+    author="Akihiro Kuno",
+    author_email="akuno@md.tsukuba.ac.jp",
+    description="Python module to convert SAM to MIDS format.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/akikuno/mids",
+    packages=setuptools.find_packages(where="src",),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
