2ad06f7a777117e4ae94a2d98a933c51bdabe70c
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-07-07 11:23:33 +0900

    bug fix - fix the bug occurs when the QNAME contains "@" anywhere (e.g.  "hoge@fuga").

--- src/midsv/format.py
+++ src/midsv/format.py
@@ -41,13 +41,13 @@ def check_alignments(sam: list[list]):
     """
     is_alignment = False
     for alignment in sam:
-        if "@" in alignment[0]:
+        if alignment[0].startswith("@"):
+            continue
+        if alignment[2] == "*":
             continue
         is_alignment = True
         if len(alignment) < 10:
             raise AttributeError("Alighment may not be SAM format")
-        if alignment[2] == "*":
-            continue
         idx_cstag = -1
         for i, a in enumerate(alignment):
             if a.startswith("cs:Z:="):
@@ -101,7 +101,7 @@ def dictionarize_sam(sam: list[list]) -> list[dict]:
     """
     aligns = []
     for alignment in sam:
-        if "@" in alignment[0]:
+        if alignment[0].startswith("@"):
             continue
         if alignment[2] == "*":
             continue
