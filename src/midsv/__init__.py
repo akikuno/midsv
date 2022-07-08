d92d1d39a824c8ba7670c430d5003f22a2031c66
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-07-08 12:43:55 +0900

    add `read_jsonl` and `write_jsonl`

--- src/midsv/__init__.py
+++ src/midsv/__init__.py
@@ -3,4 +3,7 @@
 """
 
 from .main import transform
-from .format import read_sam, check_sam_format
+from .format import read_sam
+from .format import check_sam_format
+from .format import read_jsonl
+from .format import write_jsonl
