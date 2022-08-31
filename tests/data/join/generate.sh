9307adadd5e9b47af031d4dfe5966bf9137b433e
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-08-31 15:20:19 +0900

    remove unused test data

--- tests/data/join/generate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-ref=tests/data/random_1500bp.fa
-que=tests/data/join/inversion_deletion.fq
-
-# minimap2 -ax map-ont --cs "$ref" "$que" >tests/data/join/inv_del_cs.sam
-minimap2 -ax map-ont --cs=long "$ref" "$que" >tests/data/join/inv_del_cslong.sam
